7c407354608088d0ef3f17bac3993f938086c777
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-06-01 02:48:37 +0400

    update on Thu, Jun  1, 2023  2:48:37 AM by ahmed on AAMSThinkPadX1 for first version, Lambda, S3 and API

--- update_git.sh
+++ update_git.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+message="first version, Lambda, S3 and API"
 git status
 
 git add .
 
-git commit -m "update on $(date) by $(whoami) on $(hostname)"
+git commit -m "update on $(date) by $(whoami) on $(hostname) for $message"
 
 git push origin main
 sleep 2s
