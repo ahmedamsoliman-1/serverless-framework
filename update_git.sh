15136b78aecb93195362426f2678933c2a7ec222
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-06-01 11:46:41 +0400

    Updated on branch api-keys by ahmed on AAMSThinkPadX1 for the files:  handler.js update_git.sh

--- update_git.sh
+++ update_git.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-# branch=$(git symbolic-ref --short HEAD)
-# modified_files=$(git status --porcelain | awk '{if ($1 == "M" || $1 == "A") print $2}')
-
-# commit_message="Updated or added files on branch $branch:"
-# for file in $modified_files; do
-#   commit_message+=" $file"
-# done
-
-
-
-# git status
-
-# git add .
-
-# git commit -m "Update on by $(whoami) on $(hostname) for the files: $commit_message"
-
-# # git push origin main
-
-
-
-# sleep 2s
-
-
-
-
 
 # Get the name of the current branch
 branch=$(git symbolic-ref --short HEAD)
@@ -42,3 +17,6 @@ done
 git add .
 git commit -m "$commit_message"
 git push origin $branch
+
+
+sleep 4s
